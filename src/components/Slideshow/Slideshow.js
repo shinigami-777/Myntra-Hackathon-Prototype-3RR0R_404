d54673abef90e3893579df5ad67cd9a3941687f2
sanju <sanjuraj57421@gmail.com>
2024-07-15 01:34:10 +0530

    updated

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Slideshow.css';
+import banner1 from '../../assets/banner1.jpg';
+import banner2 from '../../assets/banner2.jpg';
+import banner3 from '../../assets/banner3.jpg';
 
 function Slideshow() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slides = [
-    {
-      image: '../../assets/banner1.jpg',
-      alt: 'Slide 1',
-    },
-    {
-      image: '../../assets/banner2.jpg',
-      alt: 'Slide 2',
-    },
-    {
-      image: '../../assets/banner3.jpg',
-      alt: 'Slide 3',
-    },
+    { image: banner1, alt: 'Slide 1' },
+    { image: banner2, alt: 'Slide 2' },
+    { image: banner3, alt: 'Slide 3' },
   ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    }, 3000); // Change slide every 3 seconds
+    return () => clearInterval(interval);
+  }, []);
 
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
@@ -26,10 +27,6 @@ function Slideshow() {
     setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1));
   };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentSlide(slideIndex);
-  };
-
   return (
     <div className="slideshow-container">
       {slides.map((slide, index) => (
@@ -37,69 +34,16 @@ function Slideshow() {
           <img src={slide.image} style={{ width: '100%' }} alt={slide.alt} />
         </div>
       ))}
-
       <a className="prev" onClick={prevSlide}>&#10094;</a>
       <a className="next" onClick={nextSlide}>&#10095;</a>
-
       <div style={{ textAlign: 'center' }}>
         {slides.map((_, index) => (
           <span
             className={`dot ${index === currentSlide ? 'active' : ''}`}
-            onClick={() => goToSlide(index)}
+            onClick={() => setCurrentSlide(index)}
             key={index}
           ></span>
         ))}
-      </div>
-
-      {/* Dropdown menus */}
-      <div className="dropdown-menu1 dropdown-menu">
-        <ul>
-          <li><a href="products.html">Shirts</a></li>
-          <li><a href="products.html">Tshirts</a></li>
-          <li><a href="products.html">Jeans</a></li>
-          <li><a href="products.html">Shoes</a></li>
-          <li><a href="products.html">Accessories</a></li>
-        </ul>
-      </div>
-
-      <div className="dropdown-menu2 dropdown-menu">
-        <ul>
-          <li><a href="products.html">Ethnic</a></li>
-          <li><a href="products.html">western wear</a></li>
-          <li><a href="products.html">Footwear</a></li>
-          <li><a href="products.html">sports wear</a></li>
-          <li><a href="products.html">Accessories</a></li>
-        </ul>
-      </div>
-
-      <div className="dropdown-menu3 dropdown-menu">
-        <ul>
-          <li><a href="products.html">Boys Clothing</a></li>
-          <li><a href="products.html">Girls Clothing</a></li>
-          <li><a href="products.html">infants</a></li>
-          <li><a href="products.html">boys footwear</a></li>
-          <li><a href="products.html">girls footwear</a></li>
-        </ul>
-      </div>
-
-      <div className="dropdown-menu4 dropdown-menu">
-        <ul>
-          <li><a href="products.html">Bed Linen</a></li>
-          <li><a href="products.html">Flooring</a></li>
-          <li><a href="products.html">bath</a></li>
-          <li><a href="products.html">home decor</a></li>
-          <li><a href="products.html">kitchen</a></li>
-        </ul>
-      </div>
-
-      <div className="dropdown-menu5 dropdown-menu">
-        <ul>
-          <li><a href="products.html">make up</a></li>
-          <li><a href="products.html">skincare</a></li>
-          <li><a href="products.html">haircare</a></li>
-          <li><a href="products.html">fragrances</a></li>
-          <li><a href="products.html">mens grooming</a></li>
-        </ul>
       </div>
     </div>
   );
